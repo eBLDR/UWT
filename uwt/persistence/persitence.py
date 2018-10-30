8bb6f2cf90dd1f4d29b805394508805dcd416790
eBLDR <ed.bldr@gmail.com>
2018-10-30 12:24:38 +0100

    CRUD transactions supported

---
+++ uwt/persistence/persitence.py
@@ -0,0 +1,53 @@
+from functools import wraps
+
+from uwt.helpers.logger import logger
+from uwt.persistence.engine import SESSION
+from uwt.persistence.models import *
+
+
+def session_manager(f):
+    """
+    Manages database connections.
+    :param f: function to be wrapped
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        session = SESSION()
+        try:
+            f()
+            session.commit()
+        except Exception as exc:
+            logger.error(exc)
+            session.rollback()
+        finally:
+            session.close()
+    return wrapper
+
+
+@session_manager
+def add_record(record):
+    """
+    Inserts new record into database.
+    :param record: SQLAlchemy object
+    """
+    session.add(record)
+
+
+@session_manager
+def update_record(record, new_data):
+    """
+    Update an existing record from the database.
+    :param record: SQLAlchemy object
+    :param new_data: fields with new values to be updated
+    """
+    record.update(new_data)
+
+
+@session_manager
+def delete_record(record):
+    """
+    Deletes a record from the database.
+    :param record: SQLAlchemy object
+    """
+    session.delete(record)
+
